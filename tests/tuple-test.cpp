c46a3fefe6c3bebd9e8d98c689df431b424698a4
Borja-PC <borja.portugal@digipen.edu>
2017-11-02 21:29:48 +0100

    Ready to start working on the tests.

--- tests/tuple-test.cpp
+++ tests/tuple-test.cpp
@@ -5,6 +5,19 @@
 
 #include "src/tuple.h"
 
+// TODOS:
+// 
+//	[] tuple can store any number of arguments
+//	[] make_tuple
+//	[] get
+//	[] tie
+//	[] tuple_cat
+//	[] tuple_element
+//	[] make_from_tuple
+//	[] forward_as_tuple
+
+
+
 
 
 
