92301a3e1355adcc10ee8b9904cb35aafb1383d4
Borja-PC <borja.portugal@digipen.edu>
2017-11-06 22:43:51 +0100

    get, make_tuple, is_tuple

--- src/tuple.h
+++ src/tuple.h
@@ -2,10 +2,10 @@
 \author Borja Portugal Martin
 */
 
-
 #pragma once
 
 #include <cstdlib>	// std::size_t
+#include <type_traits>	// std::true_type, std::false_type
 
 template <typename ... Ts>
 class tuple;
@@ -14,7 +14,7 @@ template <>
 class tuple<> {};
 
 template <typename T, typename ... Ts>
-class tuple<T, Ts ...> : public tuple<Ts...>
+class tuple<T, Ts ...> : private tuple<Ts...>
 {
 public:
 	using base = tuple<Ts...>;
@@ -24,21 +24,25 @@ public:
 	tuple() = default;
 	tuple(T && t, Ts && ... vs)
 		: base{ std::forward<Ts>(vs) ... }
-		, m_value{ std::forward<T>(t) }
+		, _value{ std::forward<T>(t) }
 	{}
 
-public:
-	T m_value;
+	T _value;
 };
 
 namespace impl
 {
+	// tuple_get_impl
 	template <std::size_t I, typename T, typename ... Ts>
 	struct tuple_get_impl
 	{
 		static auto & get(tuple<T, Ts ...> & t)
 		{
-			return tuple_get_impl<I - 1, Ts ...>::get(static_cast<tuple<Ts ...> &>(t));
+			return tuple_get_impl<I - 1, Ts ...>::get((tuple<Ts ...> &)(t));
+		}
+		static const auto & get(const tuple<T, Ts ...> & t)
+		{
+			return tuple_get_impl<I - 1, Ts ...>::get((const tuple<Ts ...> &)(t));
 		}
 	};
 
@@ -47,21 +51,55 @@ namespace impl
 	{
 		static auto & get(tuple<T, Ts ...> & t)
 		{
-			return t.m_value;
+			return t._value;
+		}
+		static const auto & get(const tuple<T, Ts ...> & t)
+		{
+			return t._value;
 		}
 	};
 
-	template <std::size_t I, typename ... Ts>
-	auto & get(tuple<Ts ...> & t)
+	// tuple_element_impl
+	template <std::size_t I, typename T>
+	struct tuple_element_impl;
+
+	template <std::size_t I, typename T, typename ... Ts>
+	struct tuple_element_impl<I, tuple<T, Ts ...>>
+		: public tuple_element_impl<I - 1, tuple<Ts ...>>
+	{};
+
+	template <typename T, typename ... Ts>
+	struct tuple_element_impl<0, tuple<T, Ts ...>>
 	{
-		return tuple_get_impl<I, Ts...>::get(t);
-	}
+		using type = T;
+	};
 }
 
+// is_tuple 
+template <typename T>
+struct is_tuple : std::false_type {};
+template <typename ... Ts>
+struct is_tuple<tuple<Ts...>> : std::true_type {};
+
+// tuple_element_t
+template <std::size_t I, typename Tuple>
+using tuple_element_t = typename ::impl::tuple_element_impl<I, Tuple>::type;
+
+// get
 template <std::size_t I, typename ... Ts>
 auto & get(tuple<Ts ...> & t)
 {
-	return ::impl::get<I>(t);
+	return ::impl::tuple_get_impl<I, Ts...>::get(t);
+}
+template <std::size_t I, typename ... Ts>
+const auto & get(const tuple<Ts ...> & t)
+{
+	return ::impl::tuple_get_impl<I, Ts...>::get(t);
 }
 
-
+// make_tuple
+template <typename ... Ts>
+auto make_tuple(Ts && ... vs)
+{
+	return tuple<Ts ...>{ std::forward<Ts>(vs) ... };
+}
--- tests/tuple-test.cpp
+++ tests/tuple-test.cpp
@@ -12,8 +12,10 @@
 // TODOS:
 // 
 //	[x] tuple can store any number of arguments
-//	[] make_tuple
+//	[x] make_tuple
+//	[] make_tuple getting references
 //	[x] get
+//	[x] is_tuple
 //	[] tie
 //	[] tuple_cat
 //	[] tuple_element
@@ -23,7 +25,21 @@
 TEST(tuple_can_take_any_type_of_arguments)
 {
 	tuple<int, float, char> my_tuple;
+	(void)my_tuple;
 }
+
+TEST(tuple_has_the_expected_size)
+{
+	struct SizeTest
+	{
+		int a;
+		float f;
+		char c;
+	};
+
+	TEST_ASSERT(sizeof(tuple<int, float, char>) == sizeof(SizeTest));
+}
+
 
 TEST(tuple_can_take_parameters_in_ctor)
 {
@@ -37,6 +53,42 @@ TEST(can_request_values_to_a_tuple)
 	TEST_ASSERT(get<1>(my_tuple) == 1.f);
 	TEST_ASSERT(get<2>(my_tuple) == 'a');
 }
+
+TEST(can_request_values_to_a_tuple_const_variation)
+{
+	const tuple<int, float, char> my_tuple{ 300, 1.f, 'a' };
+	TEST_ASSERT(get<0>(my_tuple) == 300);
+	TEST_ASSERT(get<1>(my_tuple) == 1.f);
+	TEST_ASSERT(get<2>(my_tuple) == 'a');
+}
+
+TEST(make_tuple_works_as_expected)
+{
+	const auto my_tuple = make_tuple(300, 1.f, 'a');
+	TEST_ASSERT(typeid(my_tuple) == typeid(tuple<int, float, char>));
+
+	TEST_ASSERT(get<0>(my_tuple) == 300);
+	TEST_ASSERT(get<1>(my_tuple) == 1.f);
+	TEST_ASSERT(get<2>(my_tuple) == 'a');
+}
+
+TEST(tuple_element_t_is_the_type_at_the_provided_index)
+{
+	using tuple_type = tuple<int, float, char>;
+	TEST_ASSERT(typeid(tuple_element_t<0, tuple_type>) == typeid(int));
+	TEST_ASSERT(typeid(tuple_element_t<1, tuple_type>) == typeid(float));
+	TEST_ASSERT(typeid(tuple_element_t<2, tuple_type>) == typeid(char));
+}
+
+TEST(is_tuple_evaluates_to_true_if_the_parameter_is_a_tuple)
+{
+	using tuple_type = tuple<int, float, char>;
+	TEST_ASSERT(is_tuple<tuple_type>::value);
+
+	struct Not_a_tuple {};
+	TEST_ASSERT(!is_tuple<Not_a_tuple>::value);
+}
+
 
 
 
